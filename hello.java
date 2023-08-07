bf07b1763c2c045a18c064c58504a6ef6536bf04
Anju <docgoyal001@gmail.com>
2023-08-07 11:56:47 +0000

    java modified from home

--- hello.java
+++ hello.java
@@ -2,17 +2,6 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
-   }
-/ Hello.java
-public class Hello {
-   public static void main(String[] args) {
-      System.out.println("Hello, world from GIT!");
-   }
-[A
-
-/ Hello.java
-public class Hello {
-   public static void main(String[] args) {
-      System.out.println("Hello, world from GIT!");
        System.out.println("World is amazing");
-   }
+  }
+    }
